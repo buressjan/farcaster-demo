2af2f95aaa34e1668b51e5552a26d6293bcba2b1
Jan Bures <buress.jan@email.cz>
2025-05-27 16:40:37 +0200

    add: AddMiniApp - debug

--- src/AddMiniApp.tsx
+++ src/AddMiniApp.tsx
@@ -1,36 +1,68 @@
 import { sdk } from '@farcaster/frame-sdk';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function AddMiniApp() {
   const [appAdded, setAppAdded] = useState<
     boolean | null
   >(null);
 
+  const [miniAppClicks, setMiniAppClicks] =
+    useState<number>(0);
+
+  const [errors, setErrors] = useState<string[]>(
+    []
+  );
+
   const handleAddMiniApp = async () => {
     try {
+      console.log('adding mini app');
       await sdk.actions.addMiniApp();
       setAppAdded(true);
+      setMiniAppClicks((prev) => prev + 1);
     } catch (err: any) {
+      console.error(
+        'Error adding Mini App:',
+        err
+      );
+
+      const errorMessage =
+        err.message ||
+        err.toString() ||
+        'Unknown error';
+      setErrors((prev) => [
+        ...prev,
+        errorMessage,
+      ]);
+
       if (err.name === 'RejectedByUser') {
         setAppAdded(false);
       }
     }
   };
 
+  useEffect(() => {
+    handleAddMiniApp();
+  }, []);
+
   return (
     <div>
       <h1>
         Favorites
-        {appAdded &&
+        {appAdded !== null &&
           (appAdded ? (
-            <p>: added</p>
+            <span>: added</span>
           ) : (
-            <p>: rejected by user </p>
+            <span>: rejected by user</span>
           ))}
       </h1>
       <button onClick={handleAddMiniApp}>
-        Add Mini App
+        Add Mini App {miniAppClicks}
       </button>
+      <div>
+        {errors.map((error, index) => (
+          <p key={index}>Error: {error}</p>
+        ))}
+      </div>
     </div>
   );
 }
